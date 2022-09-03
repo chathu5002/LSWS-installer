d7d4cb039085817c3d6a748014cd2a369f33ef07
Ubuntu <ubuntu@ip-172-31-27-83.us-east-2.compute.internal>
2022-09-03 18:40:57 +0000

    First commit

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Ask the user for login details
+echo Press [1] to install PHP
+echo Press [2] to install LSWS
+echo Press [3] to configure LSWS
+echo
+read -p 'Enter choise: ' varoption
+
+echo
+
+if [ "$varoption" = "a" ]; then
+    echo "equal"
+fi
