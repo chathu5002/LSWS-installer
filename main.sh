b4b7ed1dfe074f42812116c05c53fa046c194483
Chathuranga Hashan <chathu5002@gmail.com>
2022-09-05 18:13:11 +0530

    commit

--- main.sh
+++ main.sh
@@ -12,21 +12,21 @@ read -p 'Enter choice: ' varoption
 
 if [ "$varoption" = "1" ]; then
     echo
-    echo Press [1] to install Litespeed server
+    echo Press [1] to install LSWS
     echo Press [2] to install Apache
     echo Press [3] to install Nginx
     echo
     read -p 'Enter choice: ' varserver
     echo
-    if [ "$varoption" = "1" ]; then
+    if [ "$varserver" = "1" ]; then
         echo ">>>>>>> Installing Litespeed Webserver <<<<<<<"
         wget -O - http://rpms.litespeedtech.com/debian/enable_lst_debian_repo.sh | sudo bash
         # 2>&1 file.txt would read as stderr goes to stdout (terminal), stdout goes to file, so you will see error output but normal output would go to the file.
         sudo apt update 2>&1 | tee apt-update.log
         sudo apt install openlitespeed 2>&1 | tee lsws-install.log
-    elif [ "$varoption" = "2" ]; then
+    elif [ "$varserver" = "2" ]; then
         echo ">>>>>>> Installing Apache Webserver <<<<<<<"
-    elif [ "$varoption" = "3" ]; then
+    elif [ "$varserver" = "3" ]; then
         echo ">>>>>>> Installing Nginx Webserver <<<<<<<"
     else
         echo "Invalid input"
