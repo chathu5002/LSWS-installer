096c9d5fd76b916f09999ed0bb0068080b9c00de
Chathuranga Hashan <chathu5002@gmail.com>
2022-09-09 00:21:59 +0530

    fix

--- main.sh
+++ main.sh
@@ -37,7 +37,7 @@ if [ "$varoption" = "1" ]; then
         # "$PASS_ONE" | cut -c 1-6 > PASS_TWO
         echo "$PASS_ONE" > password.txt
 
-        ENCRYPT_PASS=`/usr/local/lsws/admin/fcgi-bin/admin_php -q /usr/local/lsws/admin/misc/htpasswd.php $PASS_TWO`
+        ENCRYPT_PASS=`/usr/local/lsws/admin/fcgi-bin/admin_php -q /usr/local/lsws/admin/misc/htpasswd.php $PASS_ONE`
         echo "$ADMIN_USER:$ENCRYPT_PASS" > sudo '/usr/local/lsws/admin/conf/htpasswd'
         if [ $? -eq 0 ]; then
             echo "Administrator's username/password is updated successfully!"
