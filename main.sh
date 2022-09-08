d817cb856cc015e8480667d2c0d6e3425767c3ab
Chathuranga Hashan <chathu5002@gmail.com>
2022-09-09 00:24:00 +0530

    fix

--- main.sh
+++ main.sh
@@ -31,10 +31,10 @@ if [ "$varoption" = "1" ]; then
         sudo apt install openlitespeed
 
 
-        ADMIN_USER="admin"
+        ADMIN_USER=admin
         echo "$ADMIN_USER" > username.txt
         PASS_ONE=$(curl -s https://www.passwordrandom.com/query?command=password | cut -c 1-6)
-        # "$PASS_ONE" | cut -c 1-6 > PASS_TWO
+    
         echo "$PASS_ONE" > password.txt
 
         ENCRYPT_PASS=`/usr/local/lsws/admin/fcgi-bin/admin_php -q /usr/local/lsws/admin/misc/htpasswd.php $PASS_ONE`
