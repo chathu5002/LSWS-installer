42b8df98fee1ac1e568748b50122563a73794f90
Chathuranga Hashan <chathu5002@gmail.com>
2022-09-08 23:29:06 +0530

    password error

--- main.sh
+++ main.sh
@@ -36,7 +36,7 @@ if [ "$varoption" = "1" ]; then
         cat $PASS_ONE > password.txt
 
         ENCRYPT_PASS=`/usr/local/lsws/admin/fcgi-bin/admin_php -q /usr/local/lsws/admin/misc/htpasswd.php $PASS_ONE`
-        sudo echo "$ADMIN_USER:$ENCRYPT_PASS" > /usr/local/lsws/admin/conf/htpasswd 
+        echo "$ADMIN_USER:$ENCRYPT_PASS" > sudo /usr/local/lsws/admin/conf/htpasswd 
         if [ $? -eq 0 ]; then
             echo "Administrator's username/password is updated successfully!"
         fi
