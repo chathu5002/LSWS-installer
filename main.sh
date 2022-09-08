dd1328cf8c5d4bd42046f0414adacd5bac622e8a
Chathuranga Hashan <chathu5002@gmail.com>
2022-09-08 23:32:51 +0530

    error fix

--- main.sh
+++ main.sh
@@ -33,7 +33,7 @@ if [ "$varoption" = "1" ]; then
 
         ADMIN_USER=admin
         PASS_ONE=$(curl -s https://www.passwordrandom.com/query?command=password)
-        cat $PASS_ONE > password.txt
+        echo "$PASS_ONE" > password.txt
 
         ENCRYPT_PASS=`/usr/local/lsws/admin/fcgi-bin/admin_php -q /usr/local/lsws/admin/misc/htpasswd.php $PASS_ONE`
         echo "$ADMIN_USER:$ENCRYPT_PASS" > sudo /usr/local/lsws/admin/conf/htpasswd 
