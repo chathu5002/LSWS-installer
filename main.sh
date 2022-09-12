8fed58d599775da5a5ef8d843fb897f4ad24447d
Chathuranga Hashan <chathu5002@gmail.com>
2022-09-12 15:12:12 +0530

    Feature change

--- main.sh
+++ main.sh
@@ -7,6 +7,7 @@ echo Press [1] to install webserver
 echo Press [2] to install PHP
 echo Press [3] to install DBMS
 echo Press [4] to install WordPress
+echo Press [5] to install webserver
 echo
 read -p 'Enter choice: ' varoption
 
@@ -25,24 +26,13 @@ if [ "$varoption" = "1" ]; then
         echo "\e[1;31mInstalling Litespeed Webserver...\e[0m"
         echo
         wget -O - http://rpms.litespeedtech.com/debian/enable_lst_debian_repo.sh | sudo bash
-        # 2>&1 file.txt would read as stderr goes to stdout (terminal), stdout goes to file, so you will see error output but normal output would go to the file.
-        # sudo apt update 2>&1 | tee apt-update.log
-        # sudo apt install openlitespeed 2>&1 | tee lsws-install.log
         sudo apt update
         sudo apt install openlitespeed
-
-
-       
+        sudo sh admpass.sh
 
     elif [ "$varserver" = "2" ]; then
         # echo -e "\e[1;31mInstalling Apache Webserver...\e[0m"
         echo "\e[1;31mInstalling Apache Webserver...\e[0m"
-
-        sudo sh admpass.sh
-
-
-
-
     elif [ "$varserver" = "3" ]; then
         echo -e "\e[1;31mInstalling Nginx Webserver...\e[0m"
     else
@@ -57,6 +47,8 @@ elif [ "$varoption" = "3" ]; then
     echo "Installing DBMS"
 elif [ "$varoption" = "4" ]; then
     echo "Installing WordPress"
+elif [ "$varoption" = "5" ]; then
+    echo "Change LSWS password"
 else
     echo "Invalid input"
 fi
