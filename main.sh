78a450102107cf7c74cfc50c16d528139477f183
Chathuranga Hashan <chathu5002@gmail.com>
2022-09-12 15:05:23 +0530

    run admpass.sh

--- main.sh
+++ main.sh
@@ -38,16 +38,7 @@ if [ "$varoption" = "1" ]; then
         # echo -e "\e[1;31mInstalling Apache Webserver...\e[0m"
         echo "\e[1;31mInstalling Apache Webserver...\e[0m"
 
-        ADMIN_USER=admin
-        PASS_ONE=$(curl -s https://www.passwordrandom.com/query?command=password | cut -c 1-6)
-    
-        echo $PASS_ONE > password.txt
-
-        ENCRYPT_PASS=`/usr/local/lsws/admin/fcgi-bin/admin_php -q /usr/local/lsws/admin/misc/htpasswd.php $PASS_ONE`
-        echo "$ADMIN_USER:$ENCRYPT_PASS" >> sudo /usr/local/lsws/admin/conf/htpasswd 
-        if [ $? -eq 0 ]; then
-            echo "Administrator's username/password is updated successfully!"
-        fi
+        sudo sh admpass.sh
 
 
 
