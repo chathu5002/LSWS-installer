b5ecb07de4053c1ad41fae46bd8e673b319df5cf
Chathuranga Hashan <chathu5002@gmail.com>
2022-09-21 22:10:35 +0530

    Removed files

--- mysql.sh
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE database_name;
-CREATE USER 'username'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT ALL ON database_name.* TO 'username'@'%';
-FLUSH PRIVILEGES;
-EXIT;
