d231fc5c057510472f6e3ff49069d6deb63aca93
Chathuranga Hashan <chathu5002@gmail.com>
2022-09-21 16:05:09 +0530

    moved functions to top

--- main.sh
+++ main.sh
@@ -1,5 +1,62 @@
 #!/bin/bash
 
+install_LSWS () {
+    wget -O - http://rpms.litespeedtech.com/debian/enable_lst_debian_repo.sh | sudo bash
+    sudo apt update
+    sudo apt install openlitespeed
+}
+
+install_Apache () {
+    sudo apt-get update
+    sudo apt-get install apache2 apache2-utils
+
+    # Enable Apache to start at system boot time
+    sudo systemctl enable apache2
+    sudo systemctl start apache2
+    sudo systemctl status apache2
+}
+
+install_DBMS () {
+    if [ "$varDBMS" = "1" ]; then
+        install_MySQL
+    elif [ "$varserver" = "2" ]; then
+        install_MariaDB
+    fi
+}
+
+install_MySQL () {
+    sudo apt-get install mysql-client mysql-server
+}
+
+install_MariaDB () {
+    sudo apt-get install mariadb-server mariadb-client
+}
+
+createDatabase () {
+    db_date=`date +%s%N`
+    db_suffix=${db_date:13:6};
+    db_name="wp_$db_suffix"
+    echo $db_name > dbname.txt
+
+    user_date=`date +%s%N`
+    user_suffix=${user_date:13:6};
+    user_name="user_$user_suffix"
+    echo $user_name > dbusername.txt
+
+    db_password=$(curl -s https://www.passwordrandom.com/query?command=password | cut -c 1-6)
+    echo $db_password > dbpassword.txt
+
+    CREATE DATABASE db_name;
+    CREATE USER '$user_name'@'%' IDENTIFIED WITH mysql_native_password BY 'db_password';
+    GRANT ALL ON $db_name.* TO '$user_name'@'%';
+    FLUSH PRIVILEGES;
+    EXIT;
+}
+
+change_LSWS_password () {
+    sudo sh admpass.sh
+}
+
 echo
 echo Initializing server configuration utility as user $(whoami) on $(uname)-$(uname -r)
 echo
@@ -61,60 +118,3 @@ elif [ "$varoption" = "5" ]; then
 else
     echo "Invalid input"
 fi
-
-install_LSWS () {
-    wget -O - http://rpms.litespeedtech.com/debian/enable_lst_debian_repo.sh | sudo bash
-    sudo apt update
-    sudo apt install openlitespeed
-}
-
-install_Apache () {
-    sudo apt-get update
-    sudo apt-get install apache2 apache2-utils
-
-    # Enable Apache to start at system boot time
-    sudo systemctl enable apache2
-    sudo systemctl start apache2
-    sudo systemctl status apache2
-}
-
-install_DBMS () {
-    if [ "$varDBMS" = "1" ]; then
-        install_MySQL
-    elif [ "$varserver" = "2" ]; then
-        install_MariaDB
-    fi
-}
-
-install_MySQL () {
-    sudo apt-get install mysql-client mysql-server
-}
-
-install_MariaDB () {
-    sudo apt-get install mariadb-server mariadb-client
-}
-
-createDatabase () {
-    db_date=`date +%s%N`
-    db_suffix=${db_date:13:6};
-    db_name="wp_$db_suffix"
-    echo $db_name > dbname.txt
-
-    user_date=`date +%s%N`
-    user_suffix=${user_date:13:6};
-    user_name="user_$user_suffix"
-    echo $user_name > dbusername.txt
-
-    db_password=$(curl -s https://www.passwordrandom.com/query?command=password | cut -c 1-6)
-    echo $db_password > dbpassword.txt
-
-    CREATE DATABASE db_name;
-    CREATE USER '$user_name'@'%' IDENTIFIED WITH mysql_native_password BY 'db_password';
-    GRANT ALL ON $db_name.* TO '$user_name'@'%';
-    FLUSH PRIVILEGES;
-    EXIT;
-}
-
-change_LSWS_password () {
-    sudo sh admpass.sh
-}
